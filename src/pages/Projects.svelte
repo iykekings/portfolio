c4904c4d6c9455289e186c938be1a5b2cfd68226
Ikechukwu <iykekings36@gmail.com>
2020-01-13 13:11:45 +0100

    style fixes

--- src/pages/Projects.svelte
+++ src/pages/Projects.svelte
@@ -14,7 +14,7 @@
     display: grid;
     grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
     grid-gap: 20px;
-    padding: 2.5rem 5vw;
+    padding: 2.5rem 8vw;
     border-top: 1px solid #8080805c;
 
     article {
@@ -55,17 +55,10 @@
           &.github {
             background-color: #2a2a2a;
             color: white;
-            &:hover {
-              background-color: #03b703;
-            }
           }
           &.source {
             color: #2a2a2a;
             background-color: #f7f7f7;
-            &:hover {
-              background-color: #47aaf2;
-              color: #f7f7f7;
-            }
           }
         }
       }
@@ -84,15 +77,15 @@
       <p>{pro.description}</p>
       <div class="btn-con">
         {#if pro.link}
-          <a href={pro.source} target="blank" class="github">
-            <Icon icon="github" width="1rem" />
-            Github
-          </a>
-        {/if}
-        {#if pro.source}
           <a href={pro.link} target="blank" class="source">
             <Icon icon="link" width="1rem" />
             Link
+          </a>
+        {/if}
+        {#if pro.source}
+          <a href={pro.link} target="blank" class="github">
+            <Icon icon="github" width="1rem" />
+            Github
           </a>
         {/if}
       </div>
